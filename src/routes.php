9221d1d6a93fbe92de214845518e11f1b51bac50
Ahmed Samir <eng.ahmed.samir.fci@gmail.com>
2018-07-31 02:40:36 +0200

    Edit routes to be compatible with laravel 5.1

--- src/routes.php
+++ src/routes.php
@@ -1,22 +1,15 @@
 <?php
 
-Route::middleware(['web'])->group(function () {
-    Route::prefix('admin-monitor')->group(function () {
-        Route::get('dashboard', 'ASamir\InDbPerformanceMonitor\InDbPerformanceMonitorController@dashboard');
-        Route::get('requests', 'ASamir\InDbPerformanceMonitor\InDbPerformanceMonitorController@getRequests');
-        Route::get('request/{id}', 'ASamir\InDbPerformanceMonitor\InDbPerformanceMonitorController@showRequest');
-        Route::get('run-query/{id}', 'ASamir\InDbPerformanceMonitor\InDbPerformanceMonitorController@runQuery');
-        Route::get('archive-requests', 'ASamir\InDbPerformanceMonitor\InDbPerformanceMonitorController@archiveRequests');
-        Route::get('statistics-report', 'ASamir\InDbPerformanceMonitor\InDbPerformanceMonitorController@statisticsReport');
-        Route::get('errors-report', 'ASamir\InDbPerformanceMonitor\InDbPerformanceMonitorController@errorsReport');
-        route::get('/', 'ASamir\InDbPerformanceMonitor\InDbPerformanceMonitorController@index');
-        route::post('/', 'ASamir\InDbPerformanceMonitor\InDbPerformanceMonitorController@index');
-        route::get('logout', 'ASamir\InDbPerformanceMonitor\InDbPerformanceMonitorController@logout');
-        route::get('change-password', 'ASamir\InDbPerformanceMonitor\InDbPerformanceMonitorController@changePassword');
-        route::post('change-password', 'ASamir\InDbPerformanceMonitor\InDbPerformanceMonitorController@changePassword');
-        route::get('test', function() {
-            
-        });
-    });
-});
+Route::get('admin-monitor/dashboard', 'ASamir\InDbPerformanceMonitor\InDbPerformanceMonitorController@dashboard');
+Route::get('admin-monitor/requests', 'ASamir\InDbPerformanceMonitor\InDbPerformanceMonitorController@getRequests');
+Route::get('admin-monitor/request/{id}', 'ASamir\InDbPerformanceMonitor\InDbPerformanceMonitorController@showRequest');
+Route::get('admin-monitor/run-query/{id}', 'ASamir\InDbPerformanceMonitor\InDbPerformanceMonitorController@runQuery');
+Route::get('admin-monitor/archive-requests', 'ASamir\InDbPerformanceMonitor\InDbPerformanceMonitorController@archiveRequests');
+Route::get('admin-monitor/statistics-report', 'ASamir\InDbPerformanceMonitor\InDbPerformanceMonitorController@statisticsReport');
+Route::get('admin-monitor/errors-report', 'ASamir\InDbPerformanceMonitor\InDbPerformanceMonitorController@errorsReport');
+route::get('admin-monitor', '\ASamir\InDbPerformanceMonitor\InDbPerformanceMonitorController@index');
+route::post('admin-monitor', '\ASamir\InDbPerformanceMonitor\InDbPerformanceMonitorController@index');
+route::get('admin-monitor/logout', '\ASamir\InDbPerformanceMonitor\InDbPerformanceMonitorController@logout');
+route::get('admin-monitor/change-password', 'ASamir\InDbPerformanceMonitor\InDbPerformanceMonitorController@changePassword');
+route::post('admin-monitor/change-password', 'ASamir\InDbPerformanceMonitor\InDbPerformanceMonitorController@changePassword');
 
